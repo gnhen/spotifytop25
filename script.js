0b4bec8413418f6d562bf50231e41b64c43fb79e
gnhen <gtnhen@gmail.com>
2023-12-06 01:02:28 -0500

    Whole rework 3

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-const outputElement = document.createElement('div');
-outputElement.id = 'output';
-document.body.appendChild(outputElement);
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+    // Create outputElement and append it to the document body
+    const outputElement = document.createElement('div');
+    outputElement.id = 'output';
+    document.body.appendChild(outputElement);
 
-async function top25clicked() {
-    console.log("clicked");
+    // Define the top25clicked function
+    async function top25clicked() {
+        console.log("clicked");
 
-    try {
-        const response = await fetch('/run_script');
-        const result = await response.json();
+        try {
+            // Fetch data from the server
+            const response = await fetch('/run_script');
 
-        if (result.error) {
-            console.error('Error:', result.error);
-        } else {
-            outputElement.textContent = result.output;
-        }
-    } catch (error) {
-        if (response.status === 404) {
-            console.error('Error: Endpoint not found');
-        } else if (response.ok) {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            // Parse the JSON response
             const result = await response.json();
-            outputElement.textContent = result.output;
-        } else {
-            console.error('Error:', response.statusText);
-        }
 
+            // Update the content of the outputElement
+            outputElement.textContent = result.output;
+        } catch (error) {
+            console.error('Error:', error);
+        }
     }
-}
+
+    // Assign the top25clicked function to the button's onclick event
+    const spotifyButton = document.getElementById('spotifyButton');
+    if (spotifyButton) {
+        spotifyButton.onclick = top25clicked;
+    } else {
+        console.error('Error: Could not find the Spotify button.');
+    }
+});
