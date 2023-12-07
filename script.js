c510d7b2becaa4b319c69211e3fb5d2e4bd3c6a1
gnhen <gtnhen@gmail.com>
2023-12-07 00:44:38 -0500

    Trying to fix popup window

--- script.js
+++ script.js
@@ -4,50 +4,77 @@ let accessToken = null;  // Store the access token globally
 let refreshAttempt = 0; // Counter for refresh attempts
 
 const clientId = 'cae135cb691048e6baa3b09e67958658';  // Placeholder for your client ID
-const redirectUri = 'http://localhost:8080';  // Placeholder for your redirect URI
+const redirectUri = 'http://localhost:8080/';  // Replace with your GitHub Pages URL
 const scopes = 'user-top-read playlist-modify-public';
 
 async function authenticateWithSpotify() {
     try {
-        // Open a new window for user authentication
         const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scopes)}&response_type=code`;
         const authWindow = window.open(authUrl, 'Spotify Authentication', 'width=600,height=600');
 
-        // Wait for the user to complete authentication and Spotify to redirect back
-        const authCode = await waitForSpotifyRedirect(authWindow);
+        try {
+            const authCode = await waitForSpotifyRedirect(authWindow);
 
-        // Exchange the authorization code for an access token
-        const tokenResponse = await exchangeAuthCodeForToken(authCode);
-        if (!tokenResponse || !tokenResponse.access_token) {
-            throw new Error('Failed to obtain access token');
+            // Exchange the authorization code for an access token
+            const tokenResponse = await exchangeAuthCodeForToken(authCode);
+            if (!tokenResponse || !tokenResponse.access_token) {
+                throw new Error('Failed to obtain access token');
+            }
+
+            const accessToken = { ...tokenResponse, timestamp: Date.now() };  // Store the access token with timestamp
+            return accessToken;
+        } catch (error) {
+            console.error('Error during authentication:', error);
+            return null;
+        } finally {
+            authWindow.close();
         }
-
-        const accessToken = { ...tokenResponse, timestamp: Date.now() };  // Store the access token with timestamp
-        return accessToken;
     } catch (error) {
-        console.error('Error during authentication:', error);
+        console.error('Error opening Spotify authentication window:', error);
         return null;
     }
 }
 
+
 async function waitForSpotifyRedirect(authWindow) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
         const interval = setInterval(() => {
             try {
-                if (authWindow.location.href.startsWith(redirectUri)) {
+                if (authWindow.closed) {
                     clearInterval(interval);
-                    authWindow.close();
+                    reject(new Error('User closed the authentication window'));
+                    return;
+                }
 
-                    const url = new URL(authWindow.location.href);
-                    const authCode = url.searchParams.get('code');
-                    resolve(authCode);
+                let url;
+                try {
+                    url = new URL(authWindow.location.href);
+                } catch (error) {
+                    console.warn('Cross-origin security error. Ignoring.');
+                    return;
+                }
+
+                if (url.origin === redirectUri && url.searchParams.has('code')) {
+                    const code = url.searchParams.get('code');
+                    clearInterval(interval);
+                    resolve(code);
                 }
             } catch (error) {
-                // Ignore Cross-Origin Security errors
+                console.error('Error during redirect handling:', error);
+                clearInterval(interval);
+                reject(error);
             }
         }, 1000);
+
+        // Automatically close the popup window after 60 seconds
+        setTimeout(() => {
+            clearInterval(interval);
+            authWindow.close();
+            reject(new Error('Timeout: No authentication code received'));
+        }, 60000);
     });
 }
+
 
 async function exchangeAuthCodeForToken(authCode) {
     const clientSecret = '76b54e8ba6aa4409bbfcf6cfbe2a29b5';  // Placeholder for your client secret
@@ -69,19 +96,6 @@ async function exchangeAuthCodeForToken(authCode) {
     return data;
 }
 
-function isTokenExpired(token) {
-    if (!token || !token.timestamp || !token.expires_in) {
-        // Token information is incomplete, consider it expired
-        return true;
-    }
-
-    // Calculate the expiration time based on the timestamp and expires_in values
-    const expirationTime = token.timestamp + (token.expires_in * 1000);  // Convert seconds to milliseconds
-
-    // Check if the token is expired
-    return Date.now() > expirationTime;
-}
-
 async function top25clicked() {
     try {
         console.log('Button clicked');
@@ -96,33 +110,7 @@ async function top25clicked() {
 
         console.log('Making API request');
 
-        const apiResponse = await fetch('https://api.spotify.com/v1/me', {
-            method: 'GET',
-            headers: new Headers({
-                'Authorization': `Bearer ${token.access_token}`,
-            }),
-        });
-
-        if (!apiResponse.ok) {
-            const errorResponse = await apiResponse.json();
-            console.error('Error response from Spotify API:', errorResponse);
-
-            if (apiResponse.status === 401 && refreshAttempt < 3) {
-                // Token expired, refresh and try again (up to 3 attempts)
-                refreshAttempt++;
-                accessToken = null;
-                console.log('Token expired, refreshing...');
-                return top25clicked();
-            }
-
-            throw new Error(`HTTP error! Status: ${apiResponse.status}`);
-        }
-
-        // Reset the refresh attempt counter on successful API response
-        refreshAttempt = 0;
-
-        const data = await apiResponse.json();
-        console.log('API Response:', data);
+        // Use the access token for further API requests
 
     } catch (error) {
         console.error('Error during top25clicked:', error);
